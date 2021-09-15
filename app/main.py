6e07b9db7cc112e5dcd1aedd1155930257747c4e
Crol55 <carlosorantes77@gmail.com>
2021-09-15 10:47:21 -0600

    testeando la ruta /analizar

--- app/main.py
+++ app/main.py
@@ -27,31 +27,31 @@ def analizar():
     code = request.json['input']
     print("Me estoy ejecutando chato", code)
 
-    #newAmbitoGlobal = Ambito(None) # Este funciona como el ambito GLOBAL
-    #ast = interpretar(code)
-#
-    #print (" ================ EXECUTING FROM MAIN.PY ============================")
-    #Output.init() # Inicializamos nuestras variables globales, estas luego de ejecutar el interprete, se retornaran al frontend
-    ##try : 
-    #for instruccion in ast: 
-    #    retMain = instruccion.execute(newAmbitoGlobal)
-    #    if retMain != None: # Podria ser un (return, break, o continue) y eso no se permite a menos que este en un loop
-    #        if type(retMain) == bool: 
-    #            print ("Main.py: Una instruccion esta retornando algo distinto a None")
-    #            if retMain == False: # Hubo un error con alguna instruccion 
-    #                print("Main.py: Error en una instruccion: el interprete detuvo la ejecucion")
-    #                break
-    #        elif retMain.type == Type.BREAK: 
-    #            print("Error sintactico: Un 'break' no puede ser declarado afuera de un loop.")
-    #            break
-    #        elif retMain.type == Type.CONTINUE: 
-    #            print("Error sintactico: Un 'continue' no puede ser declarado afuera de un loop.")
-    #            break
-    ##except: 
-    # #   print("Error Fatal al ejecutar instrucciones")
-#
-#
-    #return { "estado": True, "msg": Output.salidaInterprete, "code": 200}
+    newAmbitoGlobal = Ambito(None) # Este funciona como el ambito GLOBAL
+    ast = interpretar(code)
+
+    print (" ================ EXECUTING FROM MAIN.PY ============================")
+    Output.init() # Inicializamos nuestras variables globales, estas luego de ejecutar el interprete, se retornaran al frontend
+    #try : 
+    for instruccion in ast: 
+        retMain = instruccion.execute(newAmbitoGlobal)
+        if retMain != None: # Podria ser un (return, break, o continue) y eso no se permite a menos que este en un loop
+            if type(retMain) == bool: 
+                print ("Main.py: Una instruccion esta retornando algo distinto a None")
+                if retMain == False: # Hubo un error con alguna instruccion 
+                    print("Main.py: Error en una instruccion: el interprete detuvo la ejecucion")
+                    break
+            elif retMain.type == Type.BREAK: 
+                print("Error sintactico: Un 'break' no puede ser declarado afuera de un loop.")
+                break
+            elif retMain.type == Type.CONTINUE: 
+                print("Error sintactico: Un 'continue' no puede ser declarado afuera de un loop.")
+                break
+    #except: 
+     #   print("Error Fatal al ejecutar instrucciones")
+
+
+    return { "estado": True, "msg": Output.salidaInterprete, "code": 200}
 
 
 
