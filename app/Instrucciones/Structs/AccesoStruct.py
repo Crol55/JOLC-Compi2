5559c205956bf5524e1f57279fd08c93a81ab6d4
Crol55 <carlosorantes77@gmail.com>
2021-09-20 11:59:03 -0600

    El operador punto anidado funciona correctamente

--- app/Instrucciones/Structs/AccesoStruct.py
+++ app/Instrucciones/Structs/AccesoStruct.py
@@ -1,3 +1,4 @@
+from Nativas.Type import Type
 from Tabla_Simbolos.simbolo import simbolo
 from Nativas.Return import Return
 from Abstractas.Expresion import Expresion
@@ -10,7 +11,7 @@ class AccesoStruct(Expresion): # Clase para acceder a la tabla de simbolos
     def __init__(self,identificador:str, lista_atributos, line, column, node):
         Expresion.__init__(self, line, column)
         self.identificador = identificador
-        self.lista_idAtributos = lista_atributos[0] #Acceso a variables del struct
+        self.lista_idAtributos = lista_atributos #Acceso a variables del struct
 
 
     def execute(self, ambito):
@@ -38,4 +39,28 @@ class AccesoStruct(Expresion): # Clase para acceder a la tabla de simbolos
                 Error("La variable: '{}' no existe.".format(self.identificador), self.line, self.column)
             ) 
         return  
+
+    def execute(self, ambito):
+        print("Aqui viene el momento decisivo", self.lista_idAtributos)
+        struct_temp:simbolo = ambito.getVariable(self.identificador)
+
+        if (struct_temp != None) and (struct_temp.tipoSimbolo == Type.STRUCT):  # creo que esta linea se puede quitar XD
+            # Iterar la lista (ej, raton.cola.color.edad)
+            for nombre_atributo in self.lista_idAtributos:
+                
+                #como esta en un ciclo hay que verificar que no sea None y siempre sea un struct
+                if (struct_temp != None) and (struct_temp.tipoSimbolo == Type.STRUCT):
+                    # Buscar adentro del struct la variable -> puede devolver otro struct o una variable normal
+                    if nombre_atributo in struct_temp.atributos:
+                        
+                        struct_temp = struct_temp.atributos[nombre_atributo] # Actualizamos struct_temp
+                    else: 
+                        print ("Error semantico en linea: {}, '{}' no es un atributo del struct '{}'".format(self.line, nombre_atributo, struct_temp.IdSimbolo)) 
+                        return None
+                else: 
+                    print ("Error semantico en linea: {}. '{}' no es un struct".format(self.line, struct_temp.IdSimbolo))
+                    return None
+            # Si llega aqui todo esta correcto
+            return Return(struct_temp.tipoSimbolo, struct_temp.valorSimbolo)
+        return 
         
