8c3a0794a29e45e5364e7212628ab0227518e82a
Crol55 <carlosorantes77@gmail.com>
2021-10-12 19:17:50 -0600

    fixing abstract method

--- app/Abstractas/Expresion.py
+++ app/Abstractas/Expresion.py
@@ -16,8 +16,8 @@ class Expresion(ABC):
     def execute(self, ambito): 
         pass
 
-    @abstractmethod 
-    def compile(): 
-        pass
+    #@abstractmethod 
+    #def compile(): 
+    #    pass
 
 
